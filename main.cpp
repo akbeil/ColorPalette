03f49e72c51f878368a10007b0c0a8af6386120d
Anders Beil <akbeil@csuchico.edu>
2024-02-21 11:24:54 -0800

    Added better error messages

--- main.cpp
+++ main.cpp
@@ -7,12 +7,19 @@ bool all_hex(char*);
 
 int main(int argc, char* argv[]) {
   int valid_colors = 0;
+  if(argc < 2) {
+    cout << "ERR_MISSING: One or more RGB values should be provided as arguments, sepertated by spaces" << endl;
+    return 0;
+  }
 
   for(int i=1; i < argc; i++) {
     int length = strlen(argv[i]);
     if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
       cout << "#" << argv[i] << endl;
     }  
+    else {
+      cout << length << " ERR_LENGTH: RGB values must be either 3 or 6 characters long" << endl;
+    }
   }
   return 0;
 }
@@ -24,6 +31,7 @@ bool all_hex(char* word) {
       continue;
     }
     else {
+      cout << word[i] << " ERR_VALUE: RGB values must be in the 0-F range" << endl;
       return false;
     }
   }
